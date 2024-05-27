628435a8dfacd717a8e0bbb0ecc92350cdad08e6
Mikhail Prokazov <monokinpro@gmail.com>
2024-05-27 23:25:34 +0300

    второй коммит

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,7 @@
 import time
+
+from selenium.webdriver import Keys
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,23 +14,34 @@ class Main_page(Base):
 
     # Locators
 
-    # search_form = "//*[@id='main-search-form']"
     search_form = "(//input[@type='search'])[1]"
+    categories_saws = "//span[@class='digi-ac-category__name']"
+
 
     # Getters
 
     def get_search_form(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.search_form)))
 
+    def get_categories_saws(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.categories_saws)))
+
     # Actions
 
     def click_search_form(self):
         self.get_search_form()
-        print("Click Search Form")
+        print("Клик на строку поиска")
 
     def input_search_form(self, search_form):
         self.get_search_form().send_keys(search_form)
-        print("Input search_form")
+        print("Ввод нужных данных в строку поиска")
+        # self.get_search_form().send_keys(Keys.ENTER)
+        # print("Вывод страницы с нужными бензопилами")
+
+    # def click_categories_saws(self):
+    #     self.get_categories_saws()
+    #     print("Клик на категорию бензопилы")
+
 
     # Methods
 
@@ -38,4 +52,9 @@ class Main_page(Base):
     def input_hyundai_saw(self):
         self.get_current_url()
         self.click_search_form()
-        # self.input_search_form("бензопила Hyundai)"
+        self.input_search_form("бензопила Hyundai")
+        self.get_current_url()
+
+    # def click_hyundai_saws(self):
+    #     self.click_categories_saws()
+    #     self.get_current_url()
--- tests/test_search_saws.py
+++ tests/test_search_saws.py
@@ -8,18 +8,26 @@ from pages.main_page import Main_page
 options = webdriver.ChromeOptions()
 options.page_load_strategy = 'eager'
 
+
 def test_search_saws():
     driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
-    print("Start Test Search Saw")
+    print("Старт теста по поиску бензопил марки Hyundai")
 
     mp = Main_page(driver)
     mp.open_url()
-    time.sleep(5)
+    time.sleep(2)
     print("Открылся нужный нам сайт")
 
     search_saws = Main_page(driver)
     search_saws.input_hyundai_saw()
     time.sleep(5)
+    print("Открылся список бензопил")
+
+    # hyundai_saws = Main_page(driver)
+    # hyundai_saws.click_hyundai_saws()
+    # time.sleep(3)
+    # print("Открылись все бензопилы Hyundai")
+    # time.sleep(5)
 
 
